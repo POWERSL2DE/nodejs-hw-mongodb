08339c6a38bb1d4ecf84263653b893259aa8f299
POWERSL2DE <naziksl2de@gmail.com>
2024-10-01 14:26:15 +0300

    Update calculatePaginationData.js

--- src/utils/calculatePaginationData.js
+++ src/utils/calculatePaginationData.js
@@ -1,8 +1,10 @@
 
-export const calculatePaginationData = (count, perPage, page) => {
+export const calculatePaginationData = (count, page, perPage) => {
     const totalPages = Math.ceil(count / perPage);
     const hasNextPage = Boolean(totalPages - page);
     const hasPreviousPage = page !== 1;
+
+
 
     return {
         page,
